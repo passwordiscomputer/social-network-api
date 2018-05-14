a47a132fdbe6a3536d57a458d685dac854565a23
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-05-14 13:05:27 -0700

    heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,7 @@ class ApplicationController < ActionController::API
     @current_user = AuthorizeApiRequest.call(request.headers).result
     render json: { error: 'Not Authorized' }, status: 401 unless @current_user
   end
+
+  def index
+  end
 end
