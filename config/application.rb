87d5d5b03c3e38ed25b71e7a857ab8f3f070fff8
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-05-14 12:55:54 -0700

    add stuff for heroku

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,8 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
+require 'rack'
+require 'rack/cors'
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
@@ -29,6 +31,7 @@ module SocialNetwork
                  :methods => [:get, :post, :options, :delete, :put, :patch]
       end
     end
+
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
