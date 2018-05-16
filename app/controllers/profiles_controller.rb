a692b6571b0438acda555ed875fd35acd6fdebee
Chase Bowden <chasebowden@Chases-MacBook-Pro.local>
2018-05-16 11:09:24 -0700

    add follow functionality and alter how routing and json returns work

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,13 +1,20 @@
 class ProfilesController < ApplicationController
   def index
     @profile = current_user.profile
-    @following_posts = current_user.following_users.includes(:posts).collect{|u| u.posts}.flatten
+    @posts = current_user.following_users.includes(:posts).collect{|u| u.posts}.flatten
     @following_users = current_user.following_users
     @follower_users = current_user.followers
     render :index
   end
 
-
+  def show
+    user = User.find(params[:id])
+    @profile = user.profile
+    @posts = user.posts
+    @following_users = user.following_users
+    @follower_users = user.followers
+    render :index
+  end
 
   def update
     if current_user == User.find(params[:user_id])
